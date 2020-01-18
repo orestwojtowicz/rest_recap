98549b6bbdb818af37fa0317036e2d5fdf056a8b
orestwojtowicz <damianwojtowicz94@gmail.com>
2020-01-18 09:15:45 +0100

    Initial commit

---
+++ src/main/java/com/orest/rest_recap/RestRecapApplication.java
@@ -0,0 +1,13 @@
+package com.orest.rest_recap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestRecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestRecapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orest/rest_recap/RestRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orest.rest_recap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestRecapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
